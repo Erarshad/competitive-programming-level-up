f267fc51bd0f910c8237899899d90579be27dfdd
Erarshad <www.moarshad310@gmail.com>
2022-05-10 11:24:57 +0530

    added

--- 2_dp/day1/paractice_dp/leetcode_746.java
+++ 2_dp/day1/paractice_dp/leetcode_746.java
@@ -1,5 +1,48 @@
 public class leetcode_746 {
     public static void main(String[] args) {
+        int[] cost={10,15,20};
+        int val=climbStairWithCost(cost,cost.length);
+        System.out.println(val);
+
+        System.out.println("---------------------");
+
+       val= climbStairWithCostDp(cost, cost.length,new int[cost.length+1]);
+       System.out.println(val);
         
+    }
+    private static int climbStairWithCost(int[] cost,int n)
+    {
+      if(n<=1){
+          return cost[n];
+      }
+
+
+       int onestep= climbStairWithCost(cost, n-1);
+       int twostep= climbStairWithCost(cost, n-2);
+
+       int fin=Math.min(onestep,twostep)+ (n==cost.length?0:cost[n]);
+       return  fin;
+
+
+
+    }
+
+    private static int climbStairWithCostDp(int[] cost, int n, int[] dp){
+        if(n<=1){
+            return dp[n]=cost[n];
+        }
+
+        if(dp[n]!=0){
+            return dp[n];
+        }
+  
+  
+         int onestep= climbStairWithCost(cost, n-1);
+         int twostep= climbStairWithCost(cost, n-2);
+  
+         int fin=Math.min(onestep,twostep)+ (n==cost.length?0:cost[n]);
+         return dp[n] = fin;
+  
+
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # competitive-programming-level-up
+
+![images](https://user-images.githubusercontent.com/57806993/167397927-44bb6d97-bbc2-47b9-aec9-6787d161a4c4.png)
+![Sketch](https://user-images.githubusercontent.com/57806993/167397955-0a1c7ab3-838d-4dc4-9e07-1043b3293e82.png)
