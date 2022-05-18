0d936159e982e3097a1cbc75dae759d52a0d8b91
Erarshad <www.moarshad310@gmail.com>
2022-05-18 21:56:23 +0530

    added

--- 3_tree/Binary tree/day_1/topview.java
+++ 3_tree/Binary tree/day_1/topview.java
@@ -1,5 +1,182 @@
-package Binary tree.day_1;
+
+import java.io.*;
+import java.util.*;
 
 public class topview {
+  public static class Node {
+    int data;
+    int axis;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right,int axis) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+      this.axis=axis;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null,0);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null,top.node.axis-1);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null,top.node.axis+1);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+
+
+  public static void levelOrderLineWise(Node node) {
+    System.out.println("level order line wise");
+    Queue<Node> q=new LinkedList<>();
+    q.add(node);
+
+    while(q.size()>0 ){
+
+      int len=q.size();
+      for(int i=0;i<len;i++){
+
+      Node temp=q.remove();
+
+      
+
+      System.out.print(temp.data+" ");
+
+      if(temp.left!=null){
+        q.add(temp.left);
+      }
+
+
+      if(temp.right!=null){
+        q.add(temp.right);
+      }
+
+       }
+
+        System.out.println();
+  
+    }
+
+
+
+
+    }
+
+    public static void levelOrder(Node node) {
+      System.out.println("level order");
+      Queue<Node> q=new LinkedList<>();
+      q.add(node);
+  
+      while(q.size()>0 ){
+  
+     
+  
+        Node temp=q.remove();
+  
+        
+  
+        System.out.println(temp.data+"  node @ axis"+temp.axis+":");
+  
+        if(temp.left!=null){
+          q.add(temp.left);
+        }
+  
+  
+        if(temp.right!=null){
+          q.add(temp.right);
+        }
+  
+      
+  
+
+  
+  
+  
+  
+      }
+  
+
+
     
+
+
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    Node root = construct(arr);
+    levelOrder(root);
+  //  levelOrderLineWise(root);
+  }
+
 }
