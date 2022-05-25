02d500e7f0c77b47aea36cc1e4c3705dac151b9b
Erarshad <www.moarshad310@gmail.com>
2022-05-25 16:20:18 +0530

    leetcode 124

--- 3_tree/Binary tree/day_6/Binary Tree Maximum Path Sum.java
+++ 3_tree/Binary tree/day_6/Binary Tree Maximum Path Sum.java
@@ -1,5 +1,10 @@
 // https://leetcode.com/problems/binary-tree-maximum-path-sum/
 
+
+ //do dry run in-order to understand
+
+
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -16,7 +21,47 @@
  * }
  */
 class Solution {
+    int max=Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
         
+        
+     
+        maxPathsum(root);
+        return max;
+        
     }
+    
+    private int maxPathsum(TreeNode root){
+            
+        if(root==null){
+            return 0;
+        }
+        
+        
+      int left=   maxPathsum(root.left);
+      int right=  maxPathsum(root.right);
+        
+        // if left/right will be greater than zero then we will take, if negative we will do //not take consider it zero
+        
+        left=Math.max(0,left);
+        right=Math.max(0,right);
+        
+        
+        int val=left+right+root.val;
+        
+        if(val>max){
+            max=val;
+        }
+        
+      
+        
+        
+        
+        return Math.max(left,right)+root.val;
+        
+        
+    }
+    
+    
+  
 }
